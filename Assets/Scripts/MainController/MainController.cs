2cbd964c3ec6e57ef6c0c2b686215b7300cfa1e9
ZorJinklesin <zorjinklesin@gmail.com>
2023-02-20 22:34:54 +0800

    游戏主控

---
+++ Assets/Scripts/MainController/MainController.cs
@@ -0,0 +1,72 @@
+﻿using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SceneManager.Scene;
+
+
+public class MainController : MonoBehaviour
+{
+    public static MainController Instance { get; private set; }
+
+    // 场景切换管理器
+    public SceneManager.SceneManager SceneManager { get; private set; }
+
+    public bool IsGameEnd { get; private set; }
+
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        this.SceneManager = new SceneManager.SceneManager();
+        this.IsGameEnd = true;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.SceneManager.SetScene(new StartGameScene());
+
+        //this.SceneManager.SetScene(new Level1Scene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.DetectDefeat();
+        
+    }
+
+    private void DetectDefeat()
+    {
+        
+    }
+
+    public void OnVictory()
+    {
+        
+    }
+
+    public void OnDefeat()
+    {
+        
+    }
+
+    public void SetAsChildObject(GameObject obj)
+    {
+        obj.transform.parent = this.transform;
+    }
+
+
+}
