dadac84af6938a84c405b018de9e76382e8e89a2
ZorJinklesin <zorjinklesin@gmail.com>
2023-02-26 14:29:10 +0800

    主UI-背包UI(暂时)

--- Assets/Scripts/SceneManagement/Scene/Level1Scene.cs
+++ Assets/Scripts/SceneManagement/Scene/Level1Scene.cs
@@ -5,18 +5,23 @@ using LoadSceneMode = UnityEngine.SceneManagement.LoadSceneMode;
 using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
 using UnityScene = UnityEngine.SceneManagement.Scene;
 
+using MainControl;
+using UIManagement.UIManager;
+using UIManagement.Panel;
+
 namespace SceneManagement.Scene
 {
     public class Level1Scene : BaseScene
     {
         private static string sceneName = "Level_01";
 
+        public UIManager UIManager { get; private set; }
+
         public Level1Scene(): base(sceneName) { }
 
         public override void OnEnter()
         {
             Debug.Log($"{this}：开始加载场景{this.SceneName}");
-            //this.panelManager = new PanelManager(new PanelFactory());
             UnitySceneManager.LoadScene(this.SceneName);
             UnitySceneManager.sceneLoaded += SceneLoaded;
         }
@@ -24,15 +29,17 @@ namespace SceneManagement.Scene
         public override void OnExit()
         {
             UnitySceneManager.sceneLoaded -= SceneLoaded;
-            //this.panelManager.PopAll();
+            MainController.Instance.UIManager.PopAll();
+            MainController.Instance.InputManager.PopLayer();
         }
 
         private void SceneLoaded(UnityScene scene, LoadSceneMode mode)
         {
             Debug.Log($"{this}：场景加载完毕{this.SceneName}");
 
-         
-            //this.panelManager.Push(new StartPanel());
+            UIManager = new UIManager();
+            MainController.Instance.InputManager.PushLayer();
+            MainController.Instance.UIManager.Push(new MainPanel());
         }
     }
 }
---
+++ Assets/Scripts/UIManagement/BackpackSystem/BackpackUI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+using MainControl;
+using UIManagement.UITools;
+
+namespace UIManagement.BackpackSystem
+{
+    public class BackpackUI : MonoBehaviour
+    {
+        [SerializeField]
+        private KeyCode openKeyCode = KeyCode.B;
+
+        [SerializeField]
+        private bool isAppear;
+
+        [SerializeField]
+        private float fadeTime = 1;
+
+        private UITool tool;
+
+        private void Awake()
+        {
+            isAppear = false;
+            tool = new UITool(gameObject);
+            tool.GetOrAddComponent<CanvasGroup>().interactable = false;
+            tool.GetOrAddComponent<CanvasGroup>().DOFade(0, 0);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            KeyBoardBind();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void KeyBoardBind()
+        {
+            Debug.Log($"{this}: bind openKeycode to {openKeyCode}");
+            MainController.Instance.InputManager.RegisterKeyDown(openKeyCode, OpenKeyCallBack);
+        }
+
+        private void OpenKeyCallBack()
+        {
+            SwitchActiveStatus();
+        }
+
+        private void SwitchActiveStatus()
+        {
+            if (isAppear)
+            {
+                Disappear();
+            } else
+            {
+                Appear();
+            }
+        }
+
+        private void Appear()
+        {
+            Debug.Log($"{this}: open backpack");
+            isAppear = true;
+            tool.GetOrAddComponent<CanvasGroup>().DOFade(1, 0.5f);
+            tool.GetOrAddComponent<CanvasGroup>().interactable = true;
+
+        }
+
+        private void Disappear()
+        {
+            Debug.Log($"{this}: hide backpack");
+            isAppear = false;
+            tool.GetOrAddComponent<CanvasGroup>().interactable = false;
+            tool.GetOrAddComponent<CanvasGroup>().DOFade(0, 0.5f);
+
+        }
+    }
+}
+
