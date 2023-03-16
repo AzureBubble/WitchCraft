71aaa28ea95fcfc9eab9da1fdac56440dbd79d22
Akiko <2436915396@qq.com>
2023-03-16 18:02:01 +0800

    场景资源316

---
+++ Assets/Scripts/SceneManager/Scene/LockMiniGame.cs
@@ -0,0 +1,37 @@
+using LoadSceneMode = UnityEngine.SceneManagement.LoadSceneMode;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+using UnityScene = UnityEngine.SceneManagement.Scene;
+using UnityEngine;
+
+namespace SceneManager.Scene
+{
+    public class LockMiniGame : BaseScene
+    {
+        private static string sceneName = "LockMiniGame";
+
+        public LockMiniGame() : base(sceneName)
+        {
+        }
+
+        public override void OnEnter()
+        {
+            Debug.Log($"{this}：开始加载场景{this.SceneName}");
+            //this.panelManager = new PanelManager(new PanelFactory());
+            UnitySceneManager.LoadScene(this.SceneName);
+            UnitySceneManager.sceneLoaded += SceneLoaded;
+        }
+
+        public override void OnExit()
+        {
+            UnitySceneManager.sceneLoaded -= SceneLoaded;
+            //this.panelManager.PopAll();
+        }
+
+        private void SceneLoaded(UnityScene scene, LoadSceneMode mode)
+        {
+            Debug.Log($"{this}：场景加载完毕{this.SceneName}");
+
+            //this.panelManager.Push(new StartPanel());
+        }
+    }
+}
