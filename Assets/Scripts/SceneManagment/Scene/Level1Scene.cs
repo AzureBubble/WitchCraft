7f1a2f56d15cee2e60e298185819d80a2b246afb
ZorJinklesin <zorjinklesin@gmail.com>
2023-03-18 15:11:58 +0800

    背包界面导入

--- Assets/Scripts/SceneManagment/Scene/Level1Scene.cs
+++ Assets/Scripts/SceneManagment/Scene/Level1Scene.cs
@@ -5,11 +5,17 @@ using LoadSceneMode = UnityEngine.SceneManagement.LoadSceneMode;
 using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
 using UnityScene = UnityEngine.SceneManagement.Scene;
 
+using MainControl;
+using UIManagement.UIManager;
+using UIManagement.Panel;
+
 namespace SceneManagement.Scene
 {
     public class Level1Scene : BaseScene
     {
         private static string sceneName = "Level_1";
+
+        public UIManager UIManager { get; private set; }
 
         public Level1Scene(): base(sceneName) { }
 
@@ -31,7 +37,9 @@ namespace SceneManagement.Scene
         {
             Debug.Log($"{this}：场景加载完毕{this.SceneName}");
 
-         
+            UIManager = new UIManager();
+            MainController.Instance.InputManager.PushLayer();
+            MainController.Instance.UIManager.Push(new MainPanel());
             //this.panelManager.Push(new StartPanel());
         }
     }
