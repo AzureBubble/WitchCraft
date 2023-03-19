4fc438a492363d819c65b71a66254f7d09ec7489
ZorJinklesin <zorjinklesin@gmail.com>
2023-03-19 12:55:40 +0800

    背包UI主控接口

--- Assets/Scripts/UIManagement/BackpackSystem/BackpackUI.cs
+++ Assets/Scripts/UIManagement/BackpackSystem/BackpackUI.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 using MainControl;
@@ -8,8 +10,10 @@ using UIManagement.UITools;
 
 namespace UIManagement.BackpackSystem
 {
-    public class BackpackUI : MonoBehaviour
+    public class BackpackUI : MonoBehaviour, IBackpackUI
     {
+        private static readonly string uiPath = "UIManagement/ItemUIs/";
+
         [SerializeField]
         private KeyCode openKeyCode = KeyCode.B;
 
@@ -21,30 +25,44 @@ namespace UIManagement.BackpackSystem
 
         private UITool tool;
 
+        private GameObject backPackContent;
+
         private void Awake()
         {
             isAppear = false;
             tool = new UITool(gameObject);
             tool.GetOrAddComponent<CanvasGroup>().interactable = false;
             tool.GetOrAddComponent<CanvasGroup>().DOFade(0, 0);
+            MainController.Instance.BackpackUI = this;
         }
 
         // Start is called before the first frame update
         void Start()
         {
+            backPackContent = transform.Find("Viewport/Content").gameObject;
             KeyBoardBind();
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            
         }
 
         private void KeyBoardBind()
         {
             Debug.Log($"{this}: bind openKeycode to {openKeyCode}");
             MainController.Instance.InputManager.RegisterKeyDown(openKeyCode, OpenKeyCallBack);
+            
+            Debug.Log($"{this}: bind '=' KeyCode to add an ItemUI for testing.");
+            MainController.Instance.InputManager.RegisterKeyDown(KeyCode.Equals, () =>
+            {
+                MainController.Instance.BackpackUI.AddItemUI("ItemUI", () =>
+                {
+                    Debug.Log("ItemUI clicked.");
+                    MainController.Instance.BackpackUI.RemoveItemUI("ItemUI");
+                });
+            });
         }
 
         private void OpenKeyCallBack()
@@ -78,8 +96,49 @@ namespace UIManagement.BackpackSystem
             isAppear = false;
             tool.GetOrAddComponent<CanvasGroup>().interactable = false;
             tool.GetOrAddComponent<CanvasGroup>().DOFade(0, 0.5f);
-
         }
+
+        public void AddItemUI(string name, Action action)
+        {
+            GameObject prefab = Resources.Load<GameObject>(uiPath + name);
+            if (prefab != null) 
+            {
+                GameObject item = GameObject.Instantiate(prefab, this.backPackContent.transform);
+                item.name = prefab.name;
+                Button btn = item.GetComponentInChildren<Button>();
+                btn.onClick.AddListener(()=>
+                {
+                    action();
+                });
+                Debug.Log($"{this}: backpack add an item - {item.name}");
+            }
+        }
+
+        public void RemoveItemUI(string name)
+        {
+            GameObject item = this.backPackContent.transform.Find(name).gameObject;
+            if (item != null)
+            {
+                Destroy(item);
+                Debug.Log($"{this}: backpack remove an item - {name}");
+            }
+        }
+    }
+
+
+    public interface IBackpackUI
+    {
+        /// <summary>
+        /// 给背包添加物品
+        /// </summary>
+        /// <param name="name">物品名</param>
+        /// <param name="action">回调方法</param>
+        void AddItemUI(string name, Action action);
+        /// <summary>
+        /// 给背包删除物品
+        /// </summary>
+        /// <param name="name">物品名</param>
+        void RemoveItemUI(string name);
     }
 }
 
