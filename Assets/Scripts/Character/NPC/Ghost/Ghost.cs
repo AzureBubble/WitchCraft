e3bf39fa33a562ab4098f7acca416be7fce80b80
AzureBubble <799375868@qq.com>
2023-04-08 14:05:55 +0800

    简单追逐完成

--- Assets/Scripts/Character/NPC/Ghost/Ghost.cs
+++ Assets/Scripts/Character/NPC/Ghost/Ghost.cs
@@ -5,23 +5,56 @@ using UnityEngine;
 
 public class Ghost : MonoBehaviour
 {
+    public Collider2D attackArea;
     public Transform target;
     public float moveSpeed = 3;
+    public float waitForAttack = 0.5f;
+    public float multipleSpeed = 0.01f;
+
+    private float timer;
+    private bool isWaiting = true;
 
     // Start is called before the first frame update
     private void Start()
     {
+        //TODO:注释这个方法，对话完后调用这个方法，等待一定时间后，开始追逐玩家
+        StartTime();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (!isWaiting && timer <= Time.time)
+        {
+            Move();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    private void Move()
+    {
+        attackArea.enabled = true;
+        moveSpeed += Time.deltaTime * multipleSpeed;
+        if (target)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed);
+        }
+    }
+
+    public void StartTime()
+    {
+        isWaiting = false;
+        timer = Time.time + waitForAttack;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            isWaiting = true;
             Debug.Log("You Lose!");
         }
     }
